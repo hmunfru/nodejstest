77b0a92f1264494e5da4a2c38806b05ff1f81872
Henar Munoz <henar@tid.es>
2013-02-25 14:57:07 +0100

    update port

--- hello_world_server.js
+++ hello_world_server.js
@@ -4,10 +4,10 @@ var server = http.createServer(function(request, response) {
 	response.writeHead(200, {
 		"Content-Type": "plain/text"
 	})
-	response.write("Hello World\n")
+	response.write("Hello World: Node.js test henar\n")
 	response.end()
 })
 
-server.listen(8080)
+server.listen(80)
 
 console.log("> SERVER STARTED")
